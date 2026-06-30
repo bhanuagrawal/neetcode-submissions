27f6b8aedc34bca615286161e316bb8945a590dc
Bhanu Agrawal <bhanuagrawal001@gmail.com>
2026-06-30 18:14:23 +0530

    Add: kth-smallest-integer-in-bst - submission-0

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.kt
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun kthSmallest(root: TreeNode?, k: Int): Int {
+        val count = IntArray(1){0}
+        return inorder(root, k, count)?:0
+    }
+
+    fun inorder(root: TreeNode?, k: Int, count: IntArray): Int? {
+
+        if(root == null) return null 
+
+        val a = inorder(root.left, k, count)
+        count[0]++
+        if(count[0] == k){
+            return root.`val`
+        }
+        val b = inorder(root.right, k, count)
+
+        return a?:b
+    }
+
+}
