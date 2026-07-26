0369d1e95f957ac846ed97b20634e7391f10ef1c
Bhanu Agrawal <bhanuagrawal001@gmail.com>
2026-07-26 12:24:06 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        var carry = 1
+        var i = digits.size-1
+        while(i >= 0 && carry > 0){
+            digits[i] += carry
+            carry = digits[i]/10
+            digits[i] %= 10
+            i--
+        }
+
+        return if(carry == 1){
+            digits.toMutableList().also{it.addFirst(1)}.toIntArray()
+        }else{
+            digits
+        }
+    }
+}
+
